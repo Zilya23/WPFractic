5cba2267fedb0070c999bd130aa2ceed3d616d7c
Зиля Шагиахметова <asunauki98@mail.ru>
2021-10-29 10:23:51 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public static ObservableCollection<User> users { get; set; }
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void auto_click(object sender, RoutedEventArgs e)
+        {
+            users = new ObservableCollection<User>(DataBaseConnect.connection.User.ToList());
+            var z = users.Where(a => a.Name == txt_login.Text && a.Password == txt_password.Password).FirstOrDefault();
+            if(z != null)
+            {
+                MessageBox.Show(z.Name);
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль неверный", "error", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+    }
+}
---
+++ WpfApp1/Page2.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new User();
+            a.ID_User = 5;
+            a.Name = login_txt.Text;
+            a.Password = password_txt.Text;
+            DataBaseConnect.connection.User.Add(a);
+            DataBaseConnect.connection.SaveChanges();
+            MessageBox.Show("All OK");
+            NavigationService.GoBack();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+    }
+}
